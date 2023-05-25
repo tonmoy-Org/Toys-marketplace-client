9a3e6643655232b5d6f985d16c7a6f12c4994822
Tonmoyghub <tonmoy15-5200@diu.edu.bd>
2023-05-25 17:27:12 +0600

    Initial Commit

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -23,7 +23,7 @@ const Banner = () => {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 2500,
+                    delay: 8000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
